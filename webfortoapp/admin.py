002ba1deace09e7e4e44867aba6e94bec9f32fb8
regiapriandi012 <regiapriandi012@gmail.com>
2023-03-12 21:34:06 +0700

    Modified admin

--- webfortoapp/admin.py
+++ webfortoapp/admin.py
@@ -3,16 +3,15 @@ from .models import *
 
 # Register your models here.
 class GithubAdmin(admin.ModelAdmin):
-    list_display = ('repo_name', 'programming_language', 'created_at', 'updated_at')
+    list_display = ('repo_name', 'topics', 'created_at', 'updated_at')
     list_filter = ("repo_name",)
-    search_fields = ['repo_name', 'programming_language', 'created_at', 'updated_at']
+    search_fields = ['repo_name', 'topics', 'created_at', 'updated_at']
     prepopulated_fields = {'repo_name': ('repo_name',)}
 admin.site.register(Github, GithubAdmin)
 
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'created_at', 'updated_at')
-    list_filter = ("title",)
     list_filter = ("title",)
     search_fields = ['title', 'description', 'created_at', 'updated_at']
     prepopulated_fields = {'title': ('title',)}
