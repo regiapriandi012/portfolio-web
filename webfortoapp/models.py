33a1afe3485020cec4783c73b960e5f617c5ff09
regiapriandi012 <regiapriandi012@gmail.com>
2023-03-12 17:16:18 +0700

    Remove certificate model

--- webfortoapp/models.py
+++ webfortoapp/models.py
@@ -9,27 +9,16 @@ class Github(models.Model):
     user_image = models.ImageField(upload_to='images/')
     name = models.CharField(max_length=100)
     username = models.CharField(max_length=100)
-    html_url = models.URLField(max_length=100)
+    repo_url = models.URLField(max_length=100)
+    user_url = models.URLField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.repo_name
 
+
 class Article(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000)
-    user_image = models.ImageField(upload_to='images/')
-    name = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    html_url = models.URLField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
-
-class Certificate(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     user_image = models.ImageField(upload_to='images/')
