21697073cacd7d8871a6c02e86218f154dce7a98
regiapriandi012 <regiapriandi012@gmail.com>
2023-03-12 17:16:56 +0700

    Configure admin

--- webfortoapp/admin.py
+++ webfortoapp/admin.py
@@ -3,25 +3,17 @@ from .models import *
 
 # Register your models here.
 class GithubAdmin(admin.ModelAdmin):
-    list_display = ('repo_name', 'programming_language', 'description', 'image', 'user_image', 'name', 'username', 'html_url', 'created_at', 'updated_at')
+    list_display = ('repo_name', 'programming_language', 'created_at', 'updated_at')
     list_filter = ("repo_name",)
-    search_fields = ['repo_name', 'programming_language', 'description', 'image', 'user_image', 'name', 'username', 'html_url', 'created_at', 'updated_at']
+    search_fields = ['repo_name', 'programming_language', 'created_at', 'updated_at']
     prepopulated_fields = {'repo_name': ('repo_name',)}
 admin.site.register(Github, GithubAdmin)
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'user_image', 'name', 'username', 'html_url', 'created_at', 'updated_at')
+    list_display = ('title', 'description', 'created_at', 'updated_at')
     list_filter = ("title",)
     list_filter = ("title",)
-    search_fields = ['title', 'description', 'user_image', 'name', 'username', 'html_url', 'created_at', 'updated_at']
+    search_fields = ['title', 'description', 'created_at', 'updated_at']
     prepopulated_fields = {'title': ('title',)}
 admin.site.register(Article, ArticleAdmin)
-
-class CertificateAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'user_image', 'name', 'username', 'html_url', 'created_at', 'updated_at')
-    list_filter = ("title",)
-    list_filter = ("title",)
-    search_fields = ['title', 'description', 'user_image', 'name', 'username', 'html_url', 'created_at', 'updated_at']
-    prepopulated_fields = {'title': ('title',)}
-admin.site.register(Certificate, CertificateAdmin)
