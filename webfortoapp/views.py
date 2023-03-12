c65ee11fe4cd4788fa98cb9d0b79ca7b58c31325
regiapriandi012 <regiapriandi012@gmail.com>
2023-03-12 17:15:46 +0700

    Remove certificate view

--- webfortoapp/views.py
+++ webfortoapp/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
-from .models import Github, Article, Certificate
+from .models import Github, Article
 
 # Create your views here.
 def index(request):
     github = Github.objects.all()
     article = Article.objects.all()
-    certificate = Certificate.objects.all()
 
     context = {
         'githubs': github,
         'aricles': article,
-        'certificates': certificate,
     }
     return render(request, 'webfortoapp/index.html', context)
