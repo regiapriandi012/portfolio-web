2d485b733583238f7695488f26131d2cdcf8789c
regiapriandi012 <regiapriandi012@gmail.com>
2023-03-12 21:51:20 +0700

    Change database

--- webforto/settings.py
+++ webforto/settings.py
@@ -76,15 +76,15 @@ WSGI_APPLICATION = 'webforto.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 # default
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
 # postgres docker compose
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_NAME'),
@@ -93,7 +93,7 @@ DATABASES = {
         'HOST': 'db',
         'PORT': 5432,
     }
-}"""
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
